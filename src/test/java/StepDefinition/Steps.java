5fcc7bb3d72d39c1bbcf8e7e1f100dae4d45b2bf
Joydeep R Roy <joydeepr938@IN.nam.ad.pwcinternal.com>
2018-06-04 23:51:34 +0530

    Latest changes in Drivers folder

--- src/test/java/StepDefinition/Steps.java
+++ src/test/java/StepDefinition/Steps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.When;
 
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -23,21 +24,18 @@ public class Steps {
 	@Given("^Open the Firefox and launch the application$")
 	public void open_the_Firefox_and_launch_the_application() throws Throwable {
 		System.out.println("This Step open the Chrome and launch the application.");
-		
-		System.setProperty("webdriver.chrome.driver", "C://Users//joydeepr938//Desktop//Backup//Cucumber_Workspace//CucumberWithSelenium//Driver//chromedriver_2.38.exe");
+		String homePath = System.getProperty("user.dir");
+		File file = new File(homePath + "//Drivers//chromedriver_2.38.exe");
+		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 		
 		// System.setProperty("webdriver.gecko.driver", "C://Users//joydeepr938//Desktop//Backup//Cucumber_Workspace//CucumberWithSelenium//Driver//geckodriver-v0.16.1.exe");
 		
-				ChromeOptions options = new ChromeOptions();
-				options.addArguments("headless");
-				
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");				
 				
 		driver= new ChromeDriver(options);
+		// driver= new ChromeDriver();
 		
-		//		Robot robot1= new Robot();
-		//		robot1.keyPress(KeyEvent.VK_SPACE);
-		// options.addArguments("start-maximized");
-		// driver= new FirefoxDriver();	
 		driver.manage().window().maximize();
 		driver.get("http://demo.guru99.com/v4");	
 		
@@ -49,12 +47,15 @@ public class Steps {
 		System.out.println("This step enter the Username and Password on the login page.");
 		driver.findElement(By.name("uid")).sendKeys("username12");							
 		driver.findElement(By.name("password")).sendKeys("password12");
+		Thread.sleep(5000);
 	}
 	
 	@Then("^Reset the credential$")
 	public void Reset_the_credential() throws Throwable {
 		System.out.println("This step click on the Reset button.");
-		driver.findElement(By.name("btnReset")).click();	
+		driver.findElement(By.name("btnReset")).click();
+		driver.close();
+		System.out.println("Hi there!!");
 	}
 
 }
